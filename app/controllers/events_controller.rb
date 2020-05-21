dab05f949aea937aaa314181a4f697129b77be6d
Sei <yuliya.glushchenko@cl-lab.ru>
2020-05-21 21:35:26 +0300

    add paginate

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.paginate(:page => params[:page], :per_page => 9)
   end
 
   def show
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,8 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = 'Moscow'
+    config.i18n.default_locale = :ru
   end
 end
