c9ebee51af032784ad03e8ca7b1e84fa912517a3
Sei <yuliya.glushchenko@cl-lab.ru>
2020-05-24 19:26:07 +0300

    add tinymce to events description

--- Gemfile
+++ Gemfile
@@ -26,9 +26,12 @@ gem 'jbuilder', '~> 2.7'
 # gem 'image_processing', '~> 1.2'
 
 gem 'will_paginate'
+#auth
 gem 'devise'
-
+# for images
 gem "mini_magick"
+
+gem 'tinymce-rails'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,4 +6,22 @@ $(document).ready( function(){
         });
     }
 
+    if (typeof tinyMCE != 'undefined') {
+        tinyMCE.init({
+            selector: "textarea.tinymce",
+            menubar: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+                'bold italic backcolor | alignleft aligncenter ' +
+                'alignright alignjustify | bullist numlist outdent indent | ' +
+                'removeformat | help',
+        });
+    } else {
+        setTimeout(arguments.callee, 50);
+    }
+
 });
