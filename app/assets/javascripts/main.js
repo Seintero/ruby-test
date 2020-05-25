4b25da18b3ef79a29c9e2cebbc4449f05980b89e
Sei <yuliya.glushchenko@cl-lab.ru>
2020-05-25 23:46:28 +0300

    add simple filter

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,11 @@
 $(document).ready( function(){
 
+    if($('#events-main-filter').length > 0) {
+        $('#events-main-filter input[type="radio"]').on('change', function () {
+           $(this).parent('form').submit();
+        })
+    }
+
     if($('.select2').length > 0){
         $('.select2').select2({
             language: "ru"
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,16 @@ class EventsController < ApplicationController
 
   # todo:  filter
   def index
-    @events = Event.paginate(:page => params[:page], :per_page => 9)
+    if params.has_key?("filter")
+      if (params[:filter]=="last")
+        @events = Event.where("event_date < ?", DateTime.now).order("event_date DESC").paginate(:page => params[:page], :per_page => 9)
+      elsif (params[:filter]=="future")
+        @events = Event.where("event_date > ?", DateTime.now).order("event_date DESC").paginate(:page => params[:page], :per_page => 9)
+      end
+      @filter = params[:filter]
+    else
+      @events = Event.order("event_date DESC").paginate(:page => params[:page], :per_page => 9)
+    end
   end
 
   def show
