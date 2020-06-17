d69467402bb2ea5253795ed30acc7064d0071990
Sei <yuliya.glushchenko@cl-lab.ru>
2020-06-18 01:30:17 +0300

    change filter

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,20 @@
-$(document).ready( function(){
-    if($('#events-main-filter').length > 0) {
-        $('#events-main-filter input[type="radio"]').on('change', function () {
-           $(this).parent('form').submit();
-        })
+$(document).ready(function () {
+    if ($('#events-main-filter').length > 0 && window.jQuery().datetimepicker) {
+        $('#date_start').datetimepicker({
+            format: 'DD-MM-YYYY',
+            locale: 'ru',
+            icons: {
+                time: 'fa fa-clock-o',
+                date: 'fa fa-calendar',
+                up: 'fa fa-chevron-up',
+                down: 'fa fa-chevron-down',
+                previous: 'fa fa-chevron-left',
+                next: 'fa fa-chevron-right',
+                today: 'fa fa-check',
+                clear: 'fa fa-trash',
+                close: 'fa fa-times'
+            }
+        });
     }
-});
+})
+;
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,52 @@
 require 'icalendar'
 
 class EventsController < ApplicationController
-  # todo:  filter
   def index
-    if params.has_key?("filter")
-      if (params[:filter]=="last")
-        @events = Event.where("event_date < ?", DateTime.now).order("event_date DESC").paginate(:page => params[:page], :per_page => 9)
-      elsif (params[:filter]=="future")
-        @events = Event.where("event_date > ?", DateTime.now).order("event_date DESC").paginate(:page => params[:page], :per_page => 9)
+    @events = Event.all
+    sql_params = []
+    conditions = []
+
+    if params.has_key?("filter_date") && !params[:filter_date].empty?
+      if (params[:filter_date] == "last")
+        conditions << ["event_date < ?"]
+      elsif (params[:filter_date] == "future")
+        conditions << ["event_date > ?"]
       end
-      @filter = params[:filter]
-    else
-      @events = Event.order("event_date DESC").paginate(:page => params[:page], :per_page => 9)
+      sql_params << DateTime.now
     end
+
+    if params.has_key?("city") && !params[:city].empty?
+      conditions << ["city LIKE ?"]
+      sql_params << "%" + params[:city] + "%"
+    end
+
+    if params.has_key?("date_start") && !params[:date_start].empty?
+      conditions << ["DATE(event_date) = ?"]
+      sql_params << Time.parse(params[:date_start]).strftime('%Y/%m/%d')
+
+    end
+
+    @events = @events.where(conditions.join(' AND '), *sql_params)
+
+    if params.has_key?("organizer") && !params[:organizer].empty?
+      @events = @events.joins(:organizer).where('organizers.name LIKE ?', "%" + params[:organizer] + "%")
+    end
+
+    @events = @events.order("event_date DESC").paginate(:page => params[:page], :per_page => 9)
+
+    respond_to do |format|
+      format.js { render partial: 'filter' }
+      format.html { render :index }
+    end
+
   end
 
   def show
     @event = Event.find(params[:id])
 
-    respond_to do |wants|
-      wants.html
-      wants.ics do
+    respond_to do |format|
+      format.html
+      format.ics do
         calendar = Icalendar::Calendar.new
         calendar.add_event(@event.to_ics)
         calendar.publish
@@ -28,4 +54,5 @@ class EventsController < ApplicationController
       end
     end
   end
+
 end
