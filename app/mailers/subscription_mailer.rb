5c29160bcc1737cbfb2cd4e2f2ffc7e396526542
Sei <yuliya.glushchenko@cl-lab.ru>
2020-06-24 01:23:24 +0300

    create task for upcoming event

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -3,8 +3,21 @@ class SubscriptionMailer < ApplicationMailer
 
 # Greeting new subscribers
 #
-  def welcome_email
+  def welcome_emailpublic
     @email = params[:email]
     mail(to: @email, subject: 'Welcome to My Awesome Site')
+  end
+
+# Upcoming Event Alert
+#
+  def upcoming_event_alert
+    days = params[:days]
+    date = (Time.now + days * 86400).strftime("%Y-%m-%d")
+    @events = Event.find_by_date(date)
+    unless @events.blank?
+      Subscriber.all_active.each do |subscriber|
+        mail(to: subscriber.email, subject: 'Оповещение о предстоящих событиях')
+      end
+    end
   end
 end
---
+++ lib/tasks/subscribers.rake
@@ -0,0 +1,12 @@
+namespace :subscribers do
+  desc "Send an email to subscriber about future events"
+  task :notify => [:environment] do
+    puts 'start send email to subscribers'
+    begin
+      SubscriptionMailer.with(days: 1).upcoming_event_alert().deliver_now
+    rescue => e
+      puts "task stopped #{e.message}"
+    end
+    puts 'end send email to subscribers'
+  end
+end
