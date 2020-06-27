515aa7ec9c47f3842a6a72ff444ccad018756015
Sei <yuliya.glushchenko@cl-lab.ru>
2020-06-27 16:32:05 +0300

    add helper for title in layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,19 @@
 module ApplicationHelper
+  # Return 'active' class for current url
+  #
   def current_class?(test_path)
     return 'active' if request.path == test_path
     ''
+  end
+
+  # Return full title
+  #
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails test application"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
 end
